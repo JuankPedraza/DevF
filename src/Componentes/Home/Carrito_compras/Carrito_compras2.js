f39d0c2e77f7760dcfb660e6d28a343d761be9c4
unknown <zuleta-96@hotmail.com>
2021-08-09 19:38:51 -0500

    Ajuste codigo no usado

--- src/Componentes/Home/Carrito_compras/Carrito_compras2.js
+++ src/Componentes/Home/Carrito_compras/Carrito_compras2.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { AuthContext } from "../../../Contexts/Auth/Auth";
 import Navbar from "../Navbar/Navbar";
